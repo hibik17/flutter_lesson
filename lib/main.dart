bc51c6e39a1183e71dbbbd11ea4a7708f77b6b33
hibik17 <hibiki@macbook>
2022-04-08 00:44:09 +0900

    section 25

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+// the main function is the starting point for all our flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('hello world'),
+      ),
+    ),
+  );
+}
